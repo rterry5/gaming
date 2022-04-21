333d2e5a439bdd3f624c47270ed346c3c4dc3e51
Rebecca Terry <rebeccaterry@Rebeccas-MacBook-Air.local>
2022-04-21 08:32:14 -0700

    first commit

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { GamesService } from './../../services/games.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  id: any;
+
+  games: any;
+
+  searchedGames: any;
+
+  constructor(private gameService: GamesService) { }
+
+  ngOnInit(): void {
+    this.searchGames();
+  }
+
+  getGames() {
+    this.gameService.getGames().subscribe((data) => {
+      this.games = data;
+      console.log(data)
+    });
+  }
+
+  searchGames() {
+    this.gameService.searchGames().subscribe((data) => {
+      this.searchedGames = data;
+    })
+  }
+
+}
---
+++ src/app/services/games.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GamesService {
+
+  constructor(private http: HttpClient) { }
+
+
+  options = {
+    "method": "GET",
+    "hostname": "rawg-video-games-database.p.rapidapi.com",
+    "port": null,
+    "path": "/games",
+    "headers": {
+      "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
+      "X-RapidAPI-Key": "81de34a78d4a4408864ded61575c4214",
+      "useQueryString": true
+    }
+  };
+
+
+  getGames() {
+    return this.http.get<any>(`https://api.rawg.io/api/genres?key=81de34a78d4a4408864ded61575c4214`);
+  }
+
+  searchGames() {
+    return this.http.get<any>(`https://api.rawg.io/api/games?search=zelda&key=81de34a78d4a4408864ded61575c4214`);
+  }
+
+}
